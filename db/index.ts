bc190f71a357fe92eca75c2dca81760a045a8c98
Deexit1 <deexitpatel4@gmail.com>
2025-01-29 04:38:48 +0530

    Migrated the database from local to neon

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+// import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import { Pool } from "pg";
 import * as schema from "./schema";
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
+// const pool = new Pool({
+// 	connectionString: process.env.DATABASE_URL,
+// });
 
-const db = drizzle(pool, { schema });
+// const db = drizzle(pool, { schema });
+const db = drizzle(process.env.DATABASE_URL as string, { schema });
 
 export default db;
